87ad5a667f1173dae7473ab513907e68a5401a2a
Nicolas Desjardins-Lecavalier <nicolas.desjardins.l@gmail.com>
2019-02-13 20:52:27 -0500

    Add files via upload

--- trackAnalysis.m
+++ trackAnalysis.m
@@ -5,13 +5,13 @@
 % last edition : 2019-02-08
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-imagesFolder = fullfile('G:' , 'Nicolas' , '20190127scan35mm');
-numPositions = 100;
-tmin = 6; %durée d'une track minimale (h)
+imagesFolder = fullfile('G:' , 'Nicolas' , '20190212darkfield35mmON');
+numPositions = 12;
+tmin = [0.5 1 2 3 6 8 12]; %durée d'une track minimale (h)
 dt = 2/60; %durée entre deux acquisition (h)
 nt = tmin/dt;
 %theseFileNames = fullfile(rawDir,{theseFileNames(:).name});
-
+resultsFolder = {[]};
 for idx = 1:numPositions
     resultsFolder{idx} = fullfile(imagesFolder,['Results' num2str(idx-1)],'trackResult.mat');
     if ~exist(resultsFolder{idx})
@@ -23,7 +23,13 @@ for idx = 1:numPositions
         N1(idx) = tracks{idx}(end,end); %nombre de tracks initiale
         list1 = 1:N1(idx);
         L{idx} = sum(tracks{idx}(:,end) == list1,1)'; %longueur des tracks
-        selectPos1 = L{idx}>nt; %tracks plus longue
+        
+        for it = 1:length(nt)
+            selectPos_tmp = L{idx}>nt(it); %tracks plus longue
+            NperH(idx,it) = sum(selectPos_tmp); %nombre de tracks plus longues que nt
+        end
+        
+        selectPos1 = L{idx}>nt(3); %tracks plus longue
         N2(idx) = sum(selectPos1); %nombre de tracks plus longues que 2h
         selectedTrackNb{idx} = list1(selectPos1); %numéro des tracks conservées
         selectPos2 = sum(tracks{idx}(:,end) == selectedTrackNb{idx},2); %positions des tracks conservées
@@ -46,14 +52,21 @@ end
 %% évaluation du tracking
 
 %% figures
+lnplot = 2;
+for it2 = 1:length(nt)
+    nameLeg{it2} = ['plus longues que' num2str(tmin(it2)) 'h'];
+end
+
 figure
-subplot(1,2,1)
-bar([N1',N2'])
+bar([N1',NperH])
 xlabel('position')
 ylabel('nombre de tracks')
-legend('initial','plus longues que 2h')
-subplot(1,2,2)
-bar(N2./N1)
-xlabel('position')
-ylabel('proportion de tracks conservées')
-legend('initial','plus longues que 2h')
+legend(['initial',nameLeg])
+
+
+   figure 
+    bar(NperH./N1')
+    xlabel('position')
+    ylabel('proportion de tracks conservées')
+    legend(nameLeg)
+    
